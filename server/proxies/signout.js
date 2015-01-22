991418abe53d8ae62abea6c0e5c7323531754097
Julien Muetton <themouette@gmail.com>
2015-01-22 18:35:49 +0100

    Add proxy servers to pyramid app

---
+++ server/proxies/signout.js
@@ -0,0 +1,20 @@
+var proxyPath = '/signout';
+
+module.exports = function(app) {
+  // For options, see:
+  // https://github.com/nodejitsu/node-http-proxy
+  var proxy = require('http-proxy').createProxyServer({
+    xfwd: true
+  });
+  var path = require('path');
+
+  proxy.on('error', function(err, req) {
+    console.error(err, req.url);
+  });
+
+  app.use(proxyPath, function(req, res, next){
+    // include root path in proxied request
+    req.url = path.join(proxyPath, req.url);
+    proxy.web(req, res, { target: 'http://0.0.0.0:6543' });
+  });
+};
