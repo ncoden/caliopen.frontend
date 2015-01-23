66aa02bbe7eafebfa4f8e227e79bb27e135091a6
Julien Muetton <themouette@gmail.com>
2015-01-23 01:47:18 +0100

    Add bootstrap as less and javascripts

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,13 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  lessOptions: {
+    paths: [
+      'bower_components/bootstrap/less'
+    ]
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -16,5 +22,18 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+app.import('bower_components/bootstrap/js/transition.js');
+app.import('bower_components/bootstrap/js/affix.js');
+app.import('bower_components/bootstrap/js/alert.js');
+app.import('bower_components/bootstrap/js/button.js');
+app.import('bower_components/bootstrap/js/carousel.js');
+app.import('bower_components/bootstrap/js/collapse.js');
+app.import('bower_components/bootstrap/js/dropdown.js');
+app.import('bower_components/bootstrap/js/modal.js');
+// popover requires tooltips
+app.import('bower_components/bootstrap/js/tooltip.js');
+app.import('bower_components/bootstrap/js/popover.js');
+app.import('bower_components/bootstrap/js/scrollspy.js');
+app.import('bower_components/bootstrap/js/tab.js');
 
 module.exports = app.toTree();
