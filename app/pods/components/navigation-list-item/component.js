b10497048d9743a5174f754eae92a4c502140252
Julien Muetton <themouette@gmail.com>
2015-01-27 15:46:11 +0100

    Add navigation-list-item component

---
+++ app/pods/components/navigation-list-item/component.js
@@ -0,0 +1,42 @@
+/**
+ * navigation-list-item
+ * ====================
+ *
+ * A component to generate a list item, linking to a route.
+ * If the route is the current one, then the list item will have an `active`
+ * class.
+ *
+ * This is especially usefull to render [bootstrap](http://getbootstrap.com)'s
+ * navs.
+ *
+ * Sample Tabs
+ * -----------
+ *
+ * @@example
+ * ```html
+ * <ul class="nav nav-tabs">
+ *   {{#navigation-list-item route='mails.index'}}mails{{/navigation-list-item}}
+ *   {{#navigation-list-item route='contacts.index'}}contacts{{/navigation-list-item}}
+ * </ul>
+ * ```
+ * @@example
+ */
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+    tagName: 'li',
+
+    classNames: [],
+    classNameBindings: ['active'],
+
+    active: function(){
+        return this.get('childViews').isAny('active');
+    }.property('childViews.@each.active'),
+
+    route: null,
+
+    willInsertElement: function () {
+        Ember.assert('route parameter is mandatory', this.get('route'));
+    }
+});
