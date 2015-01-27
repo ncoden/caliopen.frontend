0fe572522c3f47a4119aa32850f9af7ddc9f7858
Julien Muetton <themouette@gmail.com>
2015-01-27 11:31:17 +0100

    Introduces a `main-title` component

---
+++ app/pods/components/main-title/component.js
@@ -0,0 +1,9 @@
+/**
+ * This is the main panel title.
+ *
+ * Every page should define a `main-title` to display the main title.
+ */
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+});
