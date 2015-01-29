50f5c2869aa0f4acaefc97a78b5bd0ae0efbea4b
Julien Muetton <themouette@gmail.com>
2015-01-29 12:17:04 +0100

    Display a list of messages on messages page

--- app/pods/mails/route.js
+++ app/pods/mails/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model: function () {
+        return this.store.find('message');
+    },
+
+    setupController: function(controller, model) {
+        controller.set('model', model);
+    }
 });
