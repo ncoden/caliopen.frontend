9d889099a17842c9285e037e1582b08bb6a8c92c
Julien Muetton <themouette@gmail.com>
2015-01-29 11:58:49 +0100

    Add http mocks for messages and contacts

---
+++ server/mocks/contacts.js
@@ -0,0 +1,39 @@
+module.exports = function(app) {
+  var express = require('express');
+  var contactsRouter = express.Router();
+
+  contactsRouter.get('/', function(req, res) {
+    res.send({
+      'contacts': [
+        {id: 'XYZ', firstname: 'John', lastname: 'Doe'},
+        {id: 'XXX', firstname: 'Jane', lastname: 'Doe'}
+      ]
+    });
+  });
+
+  contactsRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  contactsRouter.get('/:id', function(req, res) {
+    res.send({
+      'contacts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  contactsRouter.put('/:id', function(req, res) {
+    res.send({
+      'contacts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  contactsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/contacts', contactsRouter);
+};
---
+++ server/mocks/messages.js
@@ -0,0 +1,38 @@
+module.exports = function(app) {
+  var express = require('express');
+  var messagesRouter = express.Router();
+
+  messagesRouter.get('/', function(req, res) {
+    res.send({
+      'messages': [
+        {id: 'XYZ', title: 'Blabla'}
+      ]
+    });
+  });
+
+  messagesRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  messagesRouter.get('/:id', function(req, res) {
+    res.send({
+      'messages': {
+        id: req.params.id
+      }
+    });
+  });
+
+  messagesRouter.put('/:id', function(req, res) {
+    res.send({
+      'messages': {
+        id: req.params.id
+      }
+    });
+  });
+
+  messagesRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/messages', messagesRouter);
+};
