af16279065b354413dec8745fb29768cae2648c7
Julien Muetton <themouette@gmail.com>
2015-01-29 12:13:03 +0100

    Contacts route load contacts

--- app/pods/contacts/route.js
+++ app/pods/contacts/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model: function () {
+        return this.store.find('contact');
+    },
+
+    setupController: function(controller, model) {
+        controller.set('model', model);
+    }
 });
