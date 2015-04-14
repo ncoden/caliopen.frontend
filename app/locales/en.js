0a6dca1077e4d10e281d7e20b79cb4fcc99ea756
Julien Muetton <themouette@gmail.com>
2015-04-14 15:34:11 +0200

    Migrate to ember-intl

--- app/locales/en.js
+++ app/locales/en.js
@@ -1,71 +1,72 @@
-export default {
-    "header": {
-        "menu": {
-            "messages": "Messages",
-            "contacts": "Contacts",
-            "compose": "Compose",
-            "action_default": "Action",
-            "create_user": "Create User",
-            "account": "Account",
-            "settings": "Settings",
-            "signout": "Log Out"
-        }
-    },
+import Locale from 'ember-intl/models/locale';
+export default Locale.extend({
+  locale: 'en',
+  "header": {
+    "menu": {
+      "messages": "Messages",
+      "contacts": "Contacts",
+      "compose": "Compose",
+      "action_default": "Action",
+      "create_user": "Create User",
+      "account": "Account",
+      "settings": "Settings",
+      "signout": "Log Out"
+    }
+  },
 
-    "application": {
-        "footer": {
-            "moto": "CaliOpen, Be Good"
-        }
-    },
+  "application": {
+    "footer": {
+      "moto": "CaliOpen, Be Good"
+    }
+  },
 
-    "messages": {
-      "index": {
-        "title": "Messages",
+  "messages": {
+    "index": {
+      "title": "Messages",
+    },
+    "show": {
+      "back": "Back"
+    },
+    "compose": {
+      "title": "Compose Message"
+    }
+  },
+
+  "contacts": {
+    "index": {
+      "title": "Contacts"
+    },
+    "show": {
+      "back": "Back"
+    },
+    "new": {
+      "title": "Create Contact"
+    }
+  },
+
+  "settings": {
+    "account": {
+      "title": "About Me",
+      "menu": {
+        "profile": "Profile",
+        "privacy": "Privacy",
+        "security": "Security"
       },
-      "show": {
-        "back": "Back"
+      "profile": {
+        "default": "This is the user profile for {username}"
       },
-      "compose": {
-        "title": "Compose Message"
+      "privacy": {
+        "default": "This is the privacy settings for {username}"
+      },
+      "security": {
+        "default": "This is the security settings for {username}"
       }
     },
-
-    "contacts": {
-      "index": {
-        "title": "Contacts"
-      },
-      "show": {
-        "back": "Back"
-      },
-      "new": {
-        "title": "Create Contact"
-      }
-    },
-
-    "settings": {
-      "account": {
-        "title": "About Me",
-        "menu": {
-          "profile": "Profile",
-          "privacy": "Privacy",
-          "security": "Security"
-        },
-        "profile": {
-          "default": "This is the user profile for %@1"
-        },
-        "privacy": {
-          "default": "This is the privacy settings for %@1"
-        },
-        "security": {
-          "default": "This is the security settings for %@1"
-        }
-      },
-      "app": {
-        "title": "Application Settings",
-        "menu": {
-          "none": "No settings panel for now"
-        }
+    "app": {
+      "title": "Application Settings",
+      "menu": {
+        "none": "No settings panel for now"
       }
     }
-
-};
+  }
+});
