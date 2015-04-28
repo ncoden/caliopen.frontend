19389f0c699121c075f137500f23ee226fc8ea95
Julien Muetton <julien@muetton.me>
2015-04-28 14:36:40 +0200

    Update Contact model mapping to match Current API

--- app/pods/contact/model.js
+++ app/pods/contact/model.js
@@ -2,19 +2,28 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 
-  firstname: DS.attr('string'),
-  lastname: DS.attr('string'),
+  given_name: DS.attr('string'),
+  family_name: DS.attr('string'),
+  title: DS.attr('string'),
 
-  adresses: DS.hasMany('contact/address'),
-  phones: DS.hasMany('contact/phone'),
-  emails: DS.hasMany('contact/email'),
-  ims: DS.hasMany('contact/im'),
-  identities: DS.hasMany('contact/identity'),
-  organizations: DS.hasMany('contact/organization'),
-  keys: DS.hasMany('contact/key'),
+  adresses: DS.hasMany('contact/address', {async: true}),
+  phones: DS.hasMany('contact/phone', {async: true}),
+  emails: DS.hasMany('contact/email', {async: true}),
+  ims: DS.hasMany('contact/im', {async: true}),
+  identities: DS.hasMany('contact/identity', {async: true}),
+  organizations: DS.hasMany('contact/organization', {async: true}),
+  keys: DS.hasMany('contact/key', {async: true}),
 
   fullname: function () {
     return `${this.get('firstname')} ${this.get('lastname')}`;
-  }.property('firstname', 'lastname')
+  }.property('firstname', 'lastname'),
+
+  firstname: function () {
+    return this.get('given_name');
+  }.property('given_name'),
+
+  lastname: function () {
+    return this.get('family_name');
+  }.property('family_name')
 
 });
