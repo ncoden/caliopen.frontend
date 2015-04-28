abb01081cf372471f80016f8a3423c8bc342a686
Julien Muetton <julien@muetton.me>
2015-04-28 10:35:38 +0200

    Server uses fake but consistant data

--- server/mocks/contacts.js
+++ server/mocks/contacts.js
@@ -1,13 +1,26 @@
+var _ = require('lodash');
+
+var contacts = [
+    {"family_name": "dey", "tags": null, "given_name": null, "contact_id": "6ac99495-10de-470f-99b2-b3d38bdf00ee", "title": "dey"},
+	{"family_name": "laurent", "tags": null, "given_name": null, "contact_id": "2cf04070-13d4-4da3-8f6b-fdcdd6614a9b", "title": "laurent"},
+	{"family_name": "thomas.laurent", "tags": null, "given_name": null, "contact_id": "258d3e50-9f5f-4810-9fa1-31eb91c15537", "title": "thomas.laurent"},
+	{"family_name": "mric", "tags": null, "given_name": null, "contact_id": "1c94bcdb-a766-4658-bcc0-bf78b41863be", "title": "mric"},
+	{"family_name": "alexis", "tags": null, "given_name": null, "contact_id": "f097686a-278c-4261-9d43-d553c7bcc8b9", "title": "alexis"},
+	{"family_name": "alban", "tags": null, "given_name": null, "contact_id": "9fa46257-0390-4926-8618-a046f6f22f65", "title": "alban"},
+	{"family_name": "joseph", "tags": null, "given_name": null, "contact_id": "c3685256-42e2-4712-926a-a8e540b14b6b", "title": "joseph"},
+	{"family_name": "julien.muetton", "tags": null, "given_name": null, "contact_id": "17ecccb3-33fb-4abc-b71c-537f727f4fff", "title": "julien.muetton"},
+	{"family_name": "Muetton", "tags": null, "given_name": "Julien", "contact_id": "81c7be57-b230-4058-956b-699870a694b4", "title": "Julien Muetton"},
+	{"family_name": "caliopdev", "tags": null, "given_name": null, "contact_id": "a5e379d3-f300-40a5-97c1-9179c8f8303f", "title": "caliopdev"}
+];
+
 module.exports = function(app) {
   var express = require('express');
   var contactsRouter = express.Router();
 
   contactsRouter.get('/', function(req, res) {
     res.send({
-      'contacts': [
-        {contact_id: 'XYZ', firstname: 'John', lastname: 'Doe'},
-        {contact_id: 'XXX', firstname: 'Jane', lastname: 'Doe'}
-      ]
+      total: contacts.length,
+      contacts: contacts
     });
   });
 
@@ -16,18 +29,30 @@ module.exports = function(app) {
   });
 
   contactsRouter.get('/:id', function(req, res) {
-    res.send({
-      'contacts': {
-        contact_id: req.params.id
-      }
-    });
+    var contact = _.find({id: req.params.id});
+    if (!contact) {
+      return res
+        .status(404)
+        .send({
+          message: 'Not Found'
+        });
+    }
+    return res
+      .status(200)
+      .send({contacts: contact});
   });
 
   contactsRouter.put('/:id', function(req, res) {
+    var contact = _.find({id: req.params.id});
+    if (!contact) {
+      return res
+        .status(404)
+        .send({
+          message: 'Not Found'
+        });
+    }
     res.send({
-      'contacts': {
-        contact_id: req.params.id
-      }
+      'contacts': contact
     });
   });
 
