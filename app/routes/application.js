cab8582593406d1789780507a7e90e44c1c96c06
Julien Muetton <julien@muetton.me>
2015-05-12 16:49:26 +0200

    Application route is an authentication route

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,45 +2,49 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
-    model: function () {
-        return Ember.RSVP.hash({
-            currentUser: { name: 'John Doe' }
-        });
-    },
+  model: function () {
+    return Ember.RSVP.hash({
+      currentUser: { name: 'John Doe' }
+    });
+  },
 
-    actions: {
+  actions: {
 
-        /**
-         * Transition to an error page.
-         */
-        error: function (err) {
-            // FIXME find a way to do it cleaner
-            console.log('err:', err);
-            this.transitionTo('catchall', 'unhandled-error');
-        },
-
-        /**
-         * Invalidate session.
-         */
-        invalidateSession: function () {
-          this.get('session').invalidate();
-        }
-
-    },
     /**
-     * Default behaviour when landing to settings is to redirect to account
-     * settings.
-     *
-     * Taken from https://gist.github.com/nikpachoo/7369913#file-ember-redirect-js-L40
+     * Transition to an error page.
      */
-    redirect: function(model, transition) {
-      // If this is the index route, redirect to first item
-      if( transition.targetName === 'index' ) {
-        this.transitionTo('messages');
-      }
+    error: function (err) {
+      // FIXME find a way to do it cleaner
+      console.log('err:', err);
+      this.transitionTo('catchall', 'unhandled-error');
     },
 
-    setupController: function(controller, model) {
-        controller.set('attrs.currentUser', model.currentUser);
+    /**
+     * Invalidate session.
+     */
+    invalidateSession: function () {
+      this.get('session').invalidate();
+    },
+
+    // =======================================
+    // Implement ApplicationRouteMixin Methods
+    // =======================================
+
+  },
+  /**
+    * Default behaviour when landing to settings is to redirect to account
+    * settings.
+    *
+    * Taken from https://gist.github.com/nikpachoo/7369913#file-ember-redirect-js-L40
+    */
+  redirect: function(model, transition) {
+    // If this is the index route, redirect to first item
+    if( transition.targetName === 'index' ) {
+      this.transitionTo('messages');
     }
+  },
+
+  setupController: function(controller, model) {
+    controller.set('attrs.currentUser', model.currentUser);
+  }
 });
