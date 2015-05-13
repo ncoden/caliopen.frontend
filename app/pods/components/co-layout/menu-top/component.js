b441184dafdc35b814f117a98b73259a608f5c3d
Julien Muetton <julien@muetton.me>
2015-05-13 14:25:14 +0200

    Leverage simple-auth actions in top menu

--- app/pods/components/co-layout/menu-top/component.js
+++ app/pods/components/co-layout/menu-top/component.js
@@ -2,5 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'nav',
-  classNames: ['navbar', 'navbar-default', 'navbar-fixed-top']
+  classNames: ['navbar', 'navbar-default', 'navbar-fixed-top'],
+
+  actions: {
+    logout: function () {
+      return this.sendAction('logout');
+    },
+    signin: function () {
+      return this.sendAction('signin');
+    }
+  }
 });
