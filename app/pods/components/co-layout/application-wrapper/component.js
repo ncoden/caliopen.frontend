45ff2545c24784ca8106b3735417852b7f7d2c54
Julien Muetton <julien@muetton.me>
2015-06-25 17:22:27 +0200

    Add tabs for current view

--- app/pods/components/co-layout/application-wrapper/component.js
+++ app/pods/components/co-layout/application-wrapper/component.js
@@ -7,6 +7,28 @@ export default Ember.Component.extend({
   tagName: 'div',
   classNames: [],
 
+  privacyService: Ember.inject.service('privacy-index'),
+  importanceService: Ember.inject.service('importance'),
+  tabViewsService: Ember.inject.service('tab-views'),
+
   // attributes
-  currentApplication: null
+  currentApplication: null,
+
+  importance: Ember.computed('importanceService.max', 'importanceService.min', {
+    get: function () {
+      return {min: this.get('importanceService.min'), max: this.get('importanceService.max')};
+    }
+  }),
+
+  privacy: Ember.computed('privacyService.max', 'privacyService.min', {
+    get: function () {
+      return {min: this.get('privacyService.min'), max: this.get('privacyService.max')};
+    }
+  }),
+
+  tabList: Ember.computed('tabViewsService.tabs', {
+    get: function () {
+      return this.get('tabViewsService.tabs');
+    }
+  })
 });
