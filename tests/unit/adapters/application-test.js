81ac3fbc6660050478037b7031756c134f830cf7
Julien Muetton <julien@muetton.me>
2015-07-07 16:38:02 +0200

    Add tests for application adapter

--- tests/unit/adapters/application-test.js
+++ tests/unit/adapters/application-test.js
@@ -17,7 +17,9 @@ module('ApplicationAdapter', {
 
       // Define required services
       application.register('service:importance', Ember.Service.extend({formatRange: '10:50'}));
+      application.register('service:privacyIndex', Ember.Service.extend({formatRange: '20:70'}));
       application.inject('adapter', 'importance', 'service:importance');
+      application.inject('adapter', 'privacyIndex', 'service:privacyIndex');
 
       application.deferReadiness();
     });
@@ -27,7 +29,6 @@ module('ApplicationAdapter', {
   }
 });
 
-// Replace this with your real tests.
 test('Dummy adapter should have X-CALIOPEN-IMPORTANCE header', function(assert) {
   assert.expect(1);
   var adapter = container.lookup('adapter:dummy');
@@ -35,4 +36,42 @@ test('Dummy adapter should have X-CALIOPEN-IMPORTANCE header', function(assert) 
     assert.equal('10:50', adapter.get('headers.X-CALIOPEN-IMPORTANCE'));
   });
 });
+test('Dummy adapter should update X-CALIOPEN-IMPORTANCE header', function(assert) {
+  assert.expect(2);
+  var importance = container.lookup('service:importance');
+  var adapter = container.lookup('adapter:dummy');
 
+  Ember.run(function() {
+    assert.equal('10:50', adapter.get('headers.X-CALIOPEN-IMPORTANCE'));
+    Ember.run(function() {
+      importance.set('formatRange', '0:100');
+      Ember.run(function() {
+        assert.equal('0:100', adapter.get('headers.X-CALIOPEN-IMPORTANCE'));
+      });
+    });
+  });
+});
+
+
+test('Dummy adapter should have X-CALIOPEN-PRIVACY-INDEX header', function(assert) {
+  assert.expect(1);
+  var adapter = container.lookup('adapter:dummy');
+  Ember.run(function() {
+    assert.equal('20:70', adapter.get('headers.X-CALIOPEN-PRIVACY-INDEX'));
+  });
+});
+test('Dummy adapter should update X-CALIOPEN-PRIVACY-INDEX header', function(assert) {
+  assert.expect(2);
+  var privacyIndex = container.lookup('service:privacyIndex');
+  var adapter = container.lookup('adapter:dummy');
+
+  Ember.run(function() {
+    assert.equal('20:70', adapter.get('headers.X-CALIOPEN-PRIVACY-INDEX'));
+    Ember.run(function() {
+      privacyIndex.set('formatRange', '0:100');
+      Ember.run(function() {
+        assert.equal('0:100', adapter.get('headers.X-CALIOPEN-PRIVACY-INDEX'));
+      });
+    });
+  });
+});
