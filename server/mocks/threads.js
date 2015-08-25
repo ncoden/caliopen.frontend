de9758250283b13c855d9b5e59e94bec171b29b7
Julien Muetton <julien@muetton.me>
2015-08-25 10:52:47 +0200

    Fix server when no importance/privacy index is provided

--- server/mocks/threads.js
+++ server/mocks/threads.js
@@ -28,15 +28,15 @@ module.exports = function(app) {
   var threadsRouter = express.Router();
 
   threadsRouter.use(function(req, res, next) {
-    const importance = req.header('X-CALIOPEN-IMPORTANCE') || '0-100';
-    const privacy = req.header('X-CALIOPEN-PRIVACY-INDEX') || '0-100';
+    const importanceHeader = req.header('X-CALIOPEN-IMPORTANCE') || '0:100';
+    const privacyHeader = req.header('X-CALIOPEN-PRIVACY-INDEX') || '0:100';
     var importance = {
-      min: parseInt(importance.split(':')[0], 10),
-      max: parseInt(importance.split(':')[1], 10)
+      min: parseInt(importanceHeader.split(':')[0], 10),
+      max: parseInt(importanceHeader.split(':')[1], 10)
     };
     var privacy = {
-      min: parseInt(privacy.split(':')[0], 10),
-      max: parseInt(privacy.split(':')[1], 10)
+      min: parseInt(privacyHeader.split(':')[0], 10),
+      max: parseInt(privacyHeader.split(':')[1], 10)
     };
 
     req.threads = threads.filter(function (x) {
